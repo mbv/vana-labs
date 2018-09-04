cfc6f713ef5fc331bc5ded23a46124127ceb7187
konstantinterekhov <KonstantinTerekhov@coherentsolutions.com>
2018-09-04 18:40:39 +0300

    Updated UI

--- app.rb
+++ app.rb
@@ -22,7 +22,15 @@ end
 
 
 get '/' do
-  @labs = settings.mongo_db.find.to_a.sort_by { |lab| lab[:date] }
+  @labs = settings.mongo_db.find.to_a.sort_by { |lab| lab[:date] }.map do |lab|
+    lab[:date] = lab[:date].strftime("%d.%m.%Y %H:%M:%S")
+    lab[:processed] = lab[:processed].strftime("%d.%m.%Y %H:%M:%S")
+    lab[:git_results] = lab[:git_results].map do |git_result|
+      git_result[:time] = Time.at(git_result[:time]).strftime("%d.%m.%Y %H:%M:%S") if git_result[:time]
+      git_result
+    end
+    lab
+  end
 
   erb :index
 end
